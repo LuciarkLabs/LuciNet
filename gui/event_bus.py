924d100ca76433ecf57735b458693cceff3afa91
LuciarkLabs <LuciarkLabs@users.noreply.github.com>
2026-08-17 17:51:43 +0330

    Refactor: remove redundant EventBus class

--- gui/event_bus.py
+++ gui/event_bus.py
@@ -1,16 +1,12 @@
-from PySide6.QtCore import QObject, Signal
-
-class _EventBus(QObject):
-
-    data_changed = Signal()
-
-event_bus = _EventBus()
+# gui/event_bus.py
 
 from PySide6.QtCore import QObject, Signal
+
 
 class EventBus(QObject):
     data_changed = Signal()
 
     scan_lock_changed = Signal(bool, str)
+
 
 event_bus = EventBus()
